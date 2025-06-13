514eb22b74d2ddbc2a4b09f2b52d6644de54c848
pedrohenriqueque <108901318+pedrohenriqueque@users.noreply.github.com>
2025-06-12 21:36:05 -0300

    adicionando os endpoints de fila e cobranca

--- app/controller/email.py
+++ app/controller/email.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status
 from app.schemas.email_schema import EmailRequest
 from app.services.email_service import EmailService
 
-router = APIRouter(prefix="/enviarEmail", tags=["Email"])
+router = APIRouter(prefix="/enviarEmail", tags=["Externo"])
 
 @router.post("", status_code=status.HTTP_204_NO_CONTENT)
 def enviar_email(request: EmailRequest):
