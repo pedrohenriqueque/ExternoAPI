753c2d294a852d99384d2139432313c02f89d9e1
pedrohenriqueque <108901318+pedrohenriqueque@users.noreply.github.com>
2025-07-28 13:56:54 -0300

    Ajustando para a integração entre micro-serviços

--- app/controller/restaurar.py
+++ app/controller/restaurar.py
@@ -15,3 +15,13 @@ router = APIRouter()
 def restaurar_banco():
     restaurar_dados_iniciais()
     return Response(status_code=status.HTTP_200_OK)
+
+@router.get(
+    "/restauraDados",
+    tags=["Externo"],
+    status_code=status.HTTP_200_OK,
+    summary="Restaura o banco de dados para os dados iniciais."
+)
+def restaurar_banco():
+    restaurar_dados_iniciais()
+    return Response(status_code=status.HTTP_200_OK)
